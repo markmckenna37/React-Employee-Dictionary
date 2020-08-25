e115444bb6bd3ee78a9d602e2e33bdb167791978
Mark <youremail@gmail.comg>
2020-08-25 16:54:59 -0600

    Updates

---
+++ precache-manifest.48eb1c2ee0724830321e497964b24875.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a6145cd77036ea1234cdd16e5febfe8",
+    "url": "/React-Employee-Dictionary/index.html"
+  },
+  {
+    "revision": "9d7f166c34cf527d6bbb",
+    "url": "/React-Employee-Dictionary/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "fd2e6e4d735ff8db704c",
+    "url": "/React-Employee-Dictionary/static/css/main.18521690.chunk.css"
+  },
+  {
+    "revision": "9d7f166c34cf527d6bbb",
+    "url": "/React-Employee-Dictionary/static/js/2.1e913c2d.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/React-Employee-Dictionary/static/js/2.1e913c2d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd2e6e4d735ff8db704c",
+    "url": "/React-Employee-Dictionary/static/js/main.70fa9442.chunk.js"
+  },
+  {
+    "revision": "b561398ae895c3970e79",
+    "url": "/React-Employee-Dictionary/static/js/runtime-main.43323075.js"
+  }
+]);
